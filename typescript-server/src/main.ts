289ec92f5c90506d1b1eed5bb2d2b6e263328413
Kirill Midzyaev <givename48@yandex.ru>
2023-06-01 09:23:54 +0500

    add: game-maker reconect

--- typescript-server/src/main.ts
+++ typescript-server/src/main.ts
@@ -2,15 +2,26 @@ import { Server } from "net"; // tcp
 
 const server = new Server();
 
-server.on('connection', (socket) => {
-  console.log('Client connect', socket.address());
+server.on("connection", (socket) => {
+  console.log("Client connect", socket.address());
 
-  socket.on('data', buffer => {
-    console.log(buffer);
+  socket.on("data", (buffer) => {
+    const isPing = buffer.toString("utf-8") === "ping";
+    if (isPing) {
+      return;
+    }
+
+    
+
+    console.log(buffer)
+  });
+
+  socket.on('error', (err) => {
+    console.warn('client error hook');
+    console.log(err);
   })
-})
-
-server.listen(7184, "localhost", () => {
-  console.log('Server start');
 });
 
+server.listen(7184, "localhost", () => {
+  console.log("Server start");
+});
